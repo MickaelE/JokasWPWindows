125295e7a962c40c620eb95bf92b484b4aef18b5
Mickael Eriksson <mickael.eriksson@scott-tiger.dk>
2018-02-01 15:06:33 +0100

    Change config

--- JokasWPWindows/Program.cs
+++ JokasWPWindows/Program.cs
@@ -1,26 +1,20 @@
 ﻿using Parse;
 using System;
+using System.Configuration;
 using System.Windows.Forms;
 
 namespace JokasWPWindows
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        private static String APP_ID = "244600d975e2836676e64cb3a18ceca692a9a23c";
-        private static String APP_REST_API_ID = "a9122d5ab8f97176c51a2e5cf5273dd6543bfe93";
-        private static String CUSTOM_SERVER_PATH = "http://mickenet.ddns.net:81/parse/";// trailing slash is important
-
         [STAThread]
         static void Main()
         {
 
             ParseClient.Initialize(new ParseClient.Configuration
             {
-                ApplicationId = APP_ID,
-                Server = CUSTOM_SERVER_PATH 
+                ApplicationId = ConfigurationManager.AppSettings["APP_ID"],
+                Server = ConfigurationManager.AppSettings["CUSTOM_SERVER_PATH"]
             });
            
 
